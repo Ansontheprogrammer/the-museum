a9c9eddff383616726f85538a39b7f4ae5d0f18c
ae-allison <ansonervin@yahoo.com>
2020-08-20 05:33:04 -0500

    created art hero section

---
+++ src/components/art/artHero.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import './seamoss.styles.scss'
+
+
+const ArtHeroSection = () => {
+  const data = useStaticQuery(graphql`
+    query getArtPageContent {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/(content)/" } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              page
+              paragraphOne
+              paragraphTwo
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  let jsxToRender;
+  data.allMarkdownRemark.edges.forEach((edge, index) => {
+    const { page, paragraphOne, paragraphTwo } = edge.node.frontmatter 
+
+    if(page === 'art'){
+        jsxToRender = (
+            <div className='seamoss-heading'>
+                <p className='section-description'>{paragraphOne}</p>
+                <p className='section-description'>{paragraphTwo}</p>
+            </div>
+          )
+    }
+  })
+
+  return jsxToRender
+}
+
+export default ArtHeroSection
