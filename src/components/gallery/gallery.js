8e8756b84787f09f82de4ad32ee3175ad55a7a6d
ae-allison <ansonervin@yahoo.com>
2020-08-31 10:03:46 -0500

    Separated hero section into component

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -38,19 +38,6 @@ const Gallery = () => {
 
   return (
     <div className="Gallery">
-      <div className="Gallery-text">
-        <div>
-          <h2>The</h2>
-          <h1>Museum</h1>
-          <h3 className="description">Invest in your hair, it's the only crown you never take off.</h3>
-          <a
-            className="btn"
-            href="https://squareup.com/appointments/book/EJCF1CF5DXN4W/zooty-at-procutz-sports-barbershop-lewes-de"
-          >
-            Book an appointment now
-          </a>
-        </div>
-      </div>
       <div className="Gallery-section">
         <h1 className='section-header'>Gallery</h1>
         <GalleryImages/>
---
+++ src/components/hero/hero.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+Hr
+export const Hero = props => ( 
+    <section>
+        <div className='container'>
+          {props.content}
+            <h2>{props.smallHeading}</h2>
+            <h1>{props.heading}</h1>
+            <h3 className="description">{props.subtitle}</h3>
+            <a
+            className="btn"
+            href={props.buttonLink}
+            >
+                {props.buttonText}
+            </a>
+        </div>
+    </section>
+)
+
+Hero.defaultProps = {
+    smallHeading: 'The',
+    heading: 'Museum',
+    subtitle: "Invest in your hair, it's the only crown you never take off.",
+    buttonLink: "https://squareup.com/appointments/book/EJCF1CF5DXN4W/zooty-at-procutz-sports-barbershop-lewes-de",
+    buttonText: 'Book an appointment now',
+  }
+  
+  
+Hero.propTypes = {
+    smallHeading: PropTypes.string,
+    heading: PropTypes.string,
+    subtitle: PropTypes.string,
+    buttonLink: PropTypes.string,
+    buttonText: PropTypes.string,
+}
