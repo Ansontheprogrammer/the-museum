3f59f9d8c2784387293064770a3337b4008805d7
Tawnee Allport <allprt89@gmail.com>
2020-10-27 13:57:12 -0600

    remove unused code from cart component

--- src/components/products/components/cart.js
+++ src/components/products/components/cart.js
@@ -1,22 +1,15 @@
 import React, {useEffect, useRef, useState} from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import {TweenMax} from 'gsap'
 import '../styles/cart.styles.scss'
 
 export const Cart = (props) => {
     const quantityRef = useRef(null)
 
-    const [items, setItems] = useState(0)
     const [loading, setLoading] = useState(true)
 
-    // useEffect(() => {
-    //   const tween = TweenMax.fromTo(quantityRef.current,0.5, { y: 10 }, { y: -10, yoyo: true, repeat: -1 });
-    //   tween.play()
-    // }); 
-
     useEffect(() => {
-      setTimeout(() => setLoading(false), 1000)
+      setTimeout(() => setLoading(false), 600)
     }, [])
 
 
@@ -29,9 +22,8 @@ export const Cart = (props) => {
               className='add-cart'
               style={{visibility: loading ? 'hidden' : 'visible'}}
             />
-
-              <FontAwesomeIcon icon={faShoppingCart} className='icon'/>
-              <p ref={quantityRef} className='quantity'>{props.quantity}</p>       
+            <FontAwesomeIcon icon={faShoppingCart} className='icon'/>
+            <p ref={quantityRef} className='quantity'>{props.quantity}</p>       
           </div>
         </div>
     )
