e276613894af7e70455ad094cceaa0dc3e427364
Tawnee <allprt89@gmail.com>
2020-10-28 07:57:52 -0600

    Navbar (#17)
    * add mobile nav bar. remove mobile bottom tab files
    
    * begin desktop hamburger nav
    
    * finish hamburger nav on desktop

--- src/components/navbar/desktop-nav.js
+++ src/components/navbar/desktop-nav.js
@@ -1,36 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import Logo from "../../images/logo.png"
+import {
+  FaBars,
+  FaTimes,
+} from 'react-icons/fa'
 
 import DesktopStyles from "./desktop-nav.module.scss"
 
-const DesktopNav = () => (
-  <nav className={DesktopStyles.nav}>
-      <Link to="/" activeClassName={DesktopStyles.active}>
-        Home
-      </Link>
-      <Link to="/shop/" activeClassName={DesktopStyles.active}>
-        Shop
-      </Link>
-      <Link to="/art/" activeClassName={DesktopStyles.active}>
-        Art
-      </Link>
-      <Link to="/seamoss/" activeClassName={DesktopStyles.active}>
-        Sea Moss
-      </Link>
-      <Link to="/videos/" activeClassName={DesktopStyles.active}>
-        Videos
-      </Link>
-      <Link to="/barbers/" activeClassName={DesktopStyles.active}>
-        Barbers
-      </Link>
-      <Link to="/checkout/" activeClassName={DesktopStyles.active}>
-        Checkout
-      </Link>
-      <Link to="/about/" activeClassName={DesktopStyles.active}>
-        About
-      </Link>
-  </nav>
-)
+const DesktopNav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <header className={DesktopStyles.header}>
+
+      <button 
+        className={DesktopStyles.toggle}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Menu {isOpen ? <FaTimes /> : <FaBars />}
+      </button>
+
+      <nav className={`${DesktopStyles.nav} ${isOpen ? DesktopStyles.open : ''}`}>
+          <Link to="/" activeClassName={DesktopStyles.active}>
+            Home
+          </Link>
+          <Link to="/shop/" activeClassName={DesktopStyles.active}>
+            Shop
+          </Link>
+          <Link to="/art/" activeClassName={DesktopStyles.active}>
+            Art
+          </Link>
+          <Link to="/seamoss/" activeClassName={DesktopStyles.active}>
+            Sea Moss
+          </Link>
+          <Link to="/videos/" activeClassName={DesktopStyles.active}>
+            Videos
+          </Link>
+          <Link to="/barbers/" activeClassName={DesktopStyles.active}>
+            Barbers
+          </Link>
+          <Link to="/checkout/" activeClassName={DesktopStyles.active}>
+            Checkout
+          </Link>
+          <Link to="/about/" activeClassName={DesktopStyles.active}>
+            About
+          </Link>
+      </nav>
+    </header>
+  )
+}
 
 export default DesktopNav
