e335800e3c6ce873e526f9b0f233bf6c29f38cd7
Anson Ervin <ansonervin@yahoo.com>
2020-12-14 22:08:04 -0600

    Finished repo clean

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,21 +1,21 @@
-import React from "react"
-import Layout from "../components/layout/layout"
-import PageLayout from "../components/layout/page-layout"
-import Products from "../components/products/components/products"
-import ArtHeroSection from "../components/art/artHero"
-import { SpacingSm } from "../components/typographics"
+import React from "react";
+import Layout from "../components/layout/layout";
+import PageLayout from "../components/layout/page-layout";
+import Products from "../components/products/components/products";
+import ArtHeroSection from "../components/art/artHero";
+import { SpacingSm } from "../components/typographics";
 
 const Shop = () => (
-  <Layout designNumber={0} displayAddToCartBtn={true} >
+  <Layout designNumber={0} displayAddToCartBtn={true}>
     <PageLayout title="Art">
       <div>
         <ArtHeroSection />
-        <SpacingSm/>
+        <SpacingSm />
       </div>
-      <Products category='art' displayAddToCartBtn={true} />
-      <SpacingSm/>
+      <Products category="art" displayAddToCartBtn={true} />
+      <SpacingSm />
     </PageLayout>
-   </Layout> 
-)
+  </Layout>
+);
 
-export default Shop
+export default Shop;
